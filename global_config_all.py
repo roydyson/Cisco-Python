69a4ea0bd59e2cc387c8d1525ca36e2bd36d6e2f
roydyson <45635435+roydyson@users.noreply.github.com>
2019-04-25 14:07:13 -0400

    Commit

--- global_config_all.py
+++ global_config_all.py
@@ -8,37 +8,9 @@ import yaml
 def main():
     today = datetime.now()
 
-#Load data from YAML into Python dictionary
-config_data = yaml.load(open('./newvlan.yaml'))
-show_data = yaml.load(open('./qashow.yaml'))
-
-#Load Jinja2 template
-env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
-dsw1_template = env.get_template('newvlan_dsw1.j2')
-
-env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
-dsw2_template = env.get_template('newvlan_dsw2.j2')
-
-env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
-qashow_template = env.get_template('qashow.j2')
-
-#Render the template with data and print the output
-print(dsw1_template.render(config_data))
-dsw1cfg = open( "newvlan_dsw1" + "_.cfg", "w")
-dsw1cfg.write(dsw1_template.render(config_data))
-
-print(dsw2_template.render(config_data))
-dsw2cfg = open( "newvlan_dsw2" + "_.cfg", "w")
-dsw2cfg.write(dsw2_template.render(config_data))
-
-print(qashow_template.render(config_data))
-qashow = open( "qashow" + "_.cfg", "w")
-qashow.write(qashow_template.render(show_data))
-
-with open('newvlan_dsw1_.cfg', 'r') as f:
-    dsw1_cmds = f.read().splitlines()
-
-
+with open('qashow.cfg', 'r') as f:
+    qashow = f.read().rstrip()
+print(qashow)
 
 workbook = xlrd.open_workbook(r"c:\GitHub\Cisco-Python\automation-lab.xlsx")
 
@@ -52,12 +24,29 @@ for index in range(1, sheet.nrows):
     enable_password = sheet.row(index)[4].value
     vendor = sheet.row(index)[5].value
 
-device = {
-        'device_type': vendor,
-        'ip': ipaddr,
-        'username': username,
-        'password': password,
-        'secret': enable_password,
+    device = {
+            'device_type': vendor,
+            'ip': ipaddr,
+            'username': username,
+            'password': password,
+            'secret': enable_password,
+            'global_delay_factor': 6,
 
 
-    }
+        }
+
+    print ("logging into device: ", hostname , str( datetime.now()))
+
+
+
+#for equipment in all_devices:
+    f = open("SHOW_" + hostname + ".qa", "w")
+    net_connect = ConnectHandler(**device)
+    qa_show = net_connect.send_command_timing(qashow)
+    qa_show_header = "\n++++++++++++++++++++++++++++++++++++\n"
+    print(qashow)
+
+
+    f.write(qa_show_header)
+    f.write(qa_show)
+    f.close()
